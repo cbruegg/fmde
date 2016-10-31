4e076d5dcf5907142040375aee25779f4143e416
Christian Brüggemann <mail@cbruegg.com>
2016-10-31 14:41:50 +0100

    Fix TODOs 9-12

--- FMDE/src/org/upb/fmde/de/categories/concrete/triplegraphs/TripleGraphs.java
+++ FMDE/src/org/upb/fmde/de/categories/concrete/triplegraphs/TripleGraphs.java
@@ -1,6 +1,8 @@
 package org.upb.fmde.de.categories.concrete.triplegraphs;
 
 import org.upb.fmde.de.categories.LabelledCategory;
+import org.upb.fmde.de.categories.concrete.graphs.Graph;
+import org.upb.fmde.de.categories.concrete.graphs.Graphs;
 
 public class TripleGraphs implements LabelledCategory<TripleGraph, TripleMorphism> {
 
@@ -8,13 +10,25 @@ public class TripleGraphs implements LabelledCategory<TripleGraph, TripleMorphis
 	
 	@Override
 	public TripleMorphism compose(TripleMorphism f, TripleMorphism g) {
-		// TODO (09) Composition
-		throw new UnsupportedOperationException("Has not yet been implemented.");
+		return new TripleMorphism(
+				f.label() + ";_3G" + g.label(),
+				f.src(),
+				g.trg(),
+				Graphs.Graphs.compose(f.getF_S(), g.getF_S()),
+				Graphs.Graphs.compose(f.getF_C(), g.getF_C()),
+				Graphs.Graphs.compose(f.getF_T(), g.getF_T())
+		);
 	}
 
 	@Override
 	public TripleMorphism id(TripleGraph f) {
-		// TODO (10) id
-		throw new UnsupportedOperationException("Has not yet been implemented.");
+		return new TripleMorphism(
+				"id_3G",
+				f,
+				f,
+				Graphs.Graphs.id(f.getG_S()),
+				Graphs.Graphs.id(f.getG_C()),
+				Graphs.Graphs.id(f.getG_T())
+		);
 	}
 }
--- FMDE/src/org/upb/fmde/de/categories/concrete/triplegraphs/TripleMorphism.java
+++ FMDE/src/org/upb/fmde/de/categories/concrete/triplegraphs/TripleMorphism.java
@@ -3,41 +3,42 @@ package org.upb.fmde.de.categories.concrete.triplegraphs;
 import org.upb.fmde.de.categories.ComparableArrow;
 import org.upb.fmde.de.categories.LabelledArrow;
 import org.upb.fmde.de.categories.concrete.graphs.GraphMorphism;
+import org.upb.fmde.de.categories.concrete.graphs.Graphs;
 
 public class TripleMorphism extends LabelledArrow<TripleGraph> implements ComparableArrow<TripleMorphism> {
 
-	private GraphMorphism f_S;
-	private GraphMorphism f_C;
-	private GraphMorphism f_T;
-	
-	public TripleMorphism(String label, TripleGraph source, TripleGraph target, GraphMorphism f_S, GraphMorphism f_C, GraphMorphism f_T) {
-		super(label, source, target);
-		this.f_S = f_S;
-		this.f_C = f_C;
-		this.f_T = f_T;
-		if(!isValid()) throw new IllegalArgumentException("TripleMorphism " + label + " = (" + f_S.label() + ", " + f_C.label() + ", " + f_T.label() + ") is not valid.");
-	}
+    private GraphMorphism f_S;
+    private GraphMorphism f_C;
+    private GraphMorphism f_T;
 
-	private boolean isValid() {
-		// TODO (11) Structure preservation
-		throw new UnsupportedOperationException("Has not yet been implemented.");
-	}
+    public TripleMorphism(String label, TripleGraph source, TripleGraph target, GraphMorphism f_S, GraphMorphism f_C, GraphMorphism f_T) {
+        super(label, source, target);
+        this.f_S = f_S;
+        this.f_C = f_C;
+        this.f_T = f_T;
+        if (!isValid())
+            throw new IllegalArgumentException("TripleMorphism " + label + " = (" + f_S.label() + ", " + f_C.label() + ", " + f_T.label() + ") is not valid.");
+    }
 
-	@Override
-	public boolean isTheSameAs(TripleMorphism a) {
-		// TODO (12) Equality
-		throw new UnsupportedOperationException("Has not yet been implemented.");
-	}
+    private boolean isValid() {
+        return Graphs.Graphs.compose(src().getSigma(), f_S).isTheSameAs(Graphs.Graphs.compose(f_C, trg().getSigma())) &&
+                Graphs.Graphs.compose(src().getTau(), f_T).isTheSameAs(Graphs.Graphs.compose(f_C, trg().getTau()));
+    }
 
-	public GraphMorphism getF_S() {
-		return f_S;
-	}
-	
-	public GraphMorphism getF_C() {
-		return f_C;
-	}
-	
-	public GraphMorphism getF_T() {
-		return f_T;
-	}
+    @Override
+    public boolean isTheSameAs(TripleMorphism a) {
+        return f_S.isTheSameAs(a.f_S) && f_C.isTheSameAs(a.f_C) && f_T.isTheSameAs(a.f_T);
+    }
+
+    public GraphMorphism getF_S() {
+        return f_S;
+    }
+
+    public GraphMorphism getF_C() {
+        return f_C;
+    }
+
+    public GraphMorphism getF_T() {
+        return f_T;
+    }
 }
